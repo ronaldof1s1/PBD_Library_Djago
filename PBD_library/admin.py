2836aa53d7d1c066b21d199025af5165fc0d91b3
Ronaldo Silveira <ronaldof1s1@gmail.com>
2018-05-14 23:15:40 -0300

    Create base site. with admin

--- PBD_library/admin.py
+++ PBD_library/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+class ExcludeAdmin(admin.ModelAdmin):
+    def get_model_perms(self, request):
+        return {}
+    
+admin.site.register(Address, ExcludeAdmin)
+admin.site.register(Author)
+admin.site.register(Publisher)
+admin.site.register(Book)
+admin.site.register(Copy)
+admin.site.register(User)
+admin.site.register(Loan)
--- PBD_library/models.py
+++ PBD_library/models.py
@@ -2,10 +2,19 @@ from django.db import models
 from django.utils import timezone
 # Create your models here.
 
+class Address(models.Model):
+    street = models.CharField(max_length=100)
+    number = models.PositiveIntegerField()
+    neighborhood = models.CharField(max_length=20)
+    city = models.CharField(max_length=20)
+    state = models.CharField(max_length=20)
+    country = models.CharField(max_length=20)
+
+
 class Person(models.Model):
-    name = models.CharField(unique=True)
-    address = models.CharField()
-    telephone = models.CharField()
+    name = models.CharField(unique=True, max_length=100)
+    address = models.OneToOneField(Address, on_delete=models.DO_NOTHING)
+    telephone = models.CharField(max_length=20)
 
     class Meta:
         abstract=True
@@ -14,15 +23,15 @@ class Person(models.Model):
         return self.name
 
 class Author(Person):
-    CPF = models.CharField()
+    CPF = models.CharField(max_length=14, unique=True)
     pass
 
 class Publisher(Person):
-    CNPJ = models.CharField()
+    CNPJ = models.CharField(max_length=18, unique=True)
 
 class Book(models.Model):
-    name = models.CharField()
-    keywords = models.TextField()
+    name = models.CharField(max_length=100, unique=True)
+    keywords = models.TextField(max_length=100)
     quantity = models.PositiveIntegerField()
     authors = models.ManyToManyField(Author, related_name='books')
     publisher = models.ForeignKey(Publisher, on_delete=models.DO_NOTHING, related_name='books')
@@ -30,7 +39,7 @@ class Book(models.Model):
     def __str__(self):
         return self.name
 
-class copy(models.Model):
+class Copy(models.Model):
     lent = models.BooleanField()
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='copies')
 
@@ -41,3 +50,16 @@ class copy(models.Model):
             lent = ' Disponível'
 
         return self.book.name + lent
+
+class User(Person):
+    student = models.BooleanField()
+
+class Loan(models.Model):
+    loan_date = models.DateField(default=timezone.now())
+    return_date = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    copy = models.ForeignKey(Copy, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        self.copy.__str__() + " -> " + self.user.__str__()
