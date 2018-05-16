1d3a030606eea43625343d11b1675484c10b9567
Ronaldo Silveira <ronaldof1s1@gmail.com>
2018-05-15 21:13:17 -0300

    Using forms on model admin

--- PBD_library/admin.py
+++ PBD_library/admin.py
@@ -1,15 +1,25 @@
 from django.contrib import admin
 from .models import *
+from .forms import *
 # Register your models here.
 
 class ExcludeAdmin(admin.ModelAdmin):
     def get_model_perms(self, request):
         return {}
-    
+
+
+
+class BookAdmin(admin.ModelAdmin):
+    filter_horizontal = ['authors']
+    forms = BookAdminForm
+
+class CopyAdmin(admin.ModelAdmin):
+    form = CopyAdminForm
+
 admin.site.register(Address, ExcludeAdmin)
 admin.site.register(Author)
 admin.site.register(Publisher)
-admin.site.register(Book)
-admin.site.register(Copy)
+admin.site.register(Book, BookAdmin)
+admin.site.register(Copy, CopyAdmin)
 admin.site.register(User)
 admin.site.register(Loan)
